aa57b74bd5e04c21edebd77ace5f83ba91712180
chengzx <1475290272@qq.com>
2023-10-18 13:40:19 +0800

    can train hrnet with multiband data

--- configs/hrnet/fcn_hr18_4xb4-160k_seafog_multiband-512x512.py
+++ configs/hrnet/fcn_hr18_4xb4-160k_seafog_multiband-512x512.py
@@ -2,6 +2,7 @@ _base_ = [
     '../_base_/models/fcn_hr18.py', '../_base_/datasets/seafog_multiband.py',
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
 ]
+norm_cfg = dict(type='SyncBN', requires_grad=True)
 crop_size = (512, 512)
 data_preprocessor = dict(
     type='SegDataPreProcessor',
@@ -13,5 +14,51 @@ data_preprocessor = dict(
     size=(512, 512),
     test_cfg=dict(size_divisor=32))
 model = dict(
-    in_channels=9,
-    data_preprocessor=data_preprocessor, decode_head=dict(num_classes=5))
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    # pretrained='open-mmlab://msra/hrnetv2_w18',
+    backbone=dict(
+        type='HRNet',
+        norm_cfg=norm_cfg,
+        in_channels=9,
+        norm_eval=False,
+        extra=dict(
+            stage1=dict(
+                num_modules=1,
+                num_branches=1,
+                block='BOTTLENECK',
+                num_blocks=(4, ),
+                num_channels=(64, )),
+            stage2=dict(
+                num_modules=1,
+                num_branches=2,
+                block='BASIC',
+                num_blocks=(4, 4),
+                num_channels=(18, 36)),
+            stage3=dict(
+                num_modules=4,
+                num_branches=3,
+                block='BASIC',
+                num_blocks=(4, 4, 4),
+                num_channels=(18, 36, 72)),
+            stage4=dict(
+                num_modules=3,
+                num_branches=4,
+                block='BASIC',
+                num_blocks=(4, 4, 4, 4),
+                num_channels=(18, 36, 72, 144)))),
+    decode_head=dict(
+        type='FCNHead',
+        in_channels=[18, 36, 72, 144],
+        in_index=(0, 1, 2, 3),
+        channels=sum([18, 36, 72, 144]),
+        input_transform='resize_concat',
+        kernel_size=1,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=-1,
+        num_classes=5,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),)
